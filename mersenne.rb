77a934ba02c72fdcc78f280ec2bf54a1f7cd1a7f
Greg Kubaryk <gkubaryk@vt.edu>
2011-07-30 21:45:48 -0400

    Adding comments, updating cutoff point

--- mersenne.rb
+++ mersenne.rb
@@ -10,7 +10,8 @@ File.open("full.tsv") do |f|
     factor = factor.to_i
     exponent = exponent.to_i
 
-    if bits > 100
+    # Arbitrary limit; feel free to remove, but factoring large numbers can be slow
+    if bits > 90
       puts "*** Skipping M#{exponent}'s factor (#{factor}) because #{bits} bits is too large."
       print "\n"
       next
@@ -18,10 +19,9 @@ File.open("full.tsv") do |f|
 
     text = "has factor"
 
+    # Factors take the form 2kp+1, where p is the exponent. We are interested in k.
     k = (factor - 1) / exponent / 2
     puts "M#{exponent} #{text} #{factor} (#{bits} bits)"
-    #puts "M#{exponent} #{text} #{factor} with #{bits} bits, k=#{k}"
-    #puts "M#{exponent} #{text} #{factor} (via #{type}) with #{bits} bits, k=#{k}"
     printkhash(k, factorize(k))
     print "\n"
   end
