bc92c9f327d0bc95263a3ce596ad147c0054fa36
Greg Kubaryk <gkubaryk@vt.edu>
2022-11-26 01:55:31 -0500

    remove arbitrary limit on large factors by using coreutils' factor command (if available) instead of naive ruby factoring

--- mersenne.rb
+++ mersenne.rb
@@ -4,6 +4,7 @@ require './factorize'
 File.open("full.tsv") do |f|
   total_credit = 0.0
   count = 0
+  factor_cmd_available = `bash -c 'type -P factor'`.length > 0
   f.each_line do |row|
     machine, exponent, type, date, age, factor, credit = row.split(/\t/)
 
@@ -16,19 +17,24 @@ File.open("full.tsv") do |f|
     bits = Math::log(factor)/Math::log(2)
     bits = Integer(bits * 100) / Float(100)
 
-    # Arbitrary limit; feel free to remove, but factoring large numbers can be slow
-    if bits > 125
-      puts "*** Skipping M#{exponent}'s factor (#{factor}) because #{bits} bits is too large."
-      print "\n"
-      next
-    end
-
     text = "has factor"
 
     # Factors take the form 2kp+1, where p is the exponent. We are interested in k.
     k = (factor - 1) / exponent / 2
     puts "M#{exponent} #{text} #{factor} (#{bits} bits)"
-    printkhash(k, factorize(k))
+
+    # too large to use naive ruby factoring, use factor from coreutils and hope the output format doesn't change
+    if bits > 125
+      if factor_cmd_available
+        splits = `factor #{k}`.strip.delete_prefix("#{k}: ").split(' ').map(&:to_i).tally
+        printkhash(k, splits)
+      else
+        puts "*** Skipping M#{exponent}'s factor (#{factor}) because #{bits} bits is too large."
+      end
+    else
+      printkhash(k, factorize(k))
+    end
+
     print "\n"
   end
 
